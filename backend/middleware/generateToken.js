98fa2ae94ad93930f510f29af1f7456ea4df07a1
Sam Saryon <ssaryonjr@gmail.com>
2022-09-23 05:12:45 -0700

    Added chat box ui design and timestamps

--- backend/middleware/generateToken.js
+++ backend/middleware/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: "7d",
+        expiresIn: "32d",
     });
 }
 
--- frontend/src/components/Chat Components/DisplayMessagesBox.jsx
+++ frontend/src/components/Chat Components/DisplayMessagesBox.jsx
@@ -1,14 +1,52 @@
 import React from 'react'
 import ScrollableFeed from 'react-scrollable-feed'
+import { timeDifference } from '../../config/ChatLogic';
 
-function DisplayMessagesBox({messages}) {
+function DisplayMessagesBox({ messages }) {
+  //User info
+  const currentUser = JSON.parse(localStorage.getItem("userData"));
+    const loggedUserId = currentUser._id;
+    const currentTime = new Date()
+    
   return (
-    <ScrollableFeed>
-          {messages && messages.map((m, i) => {
-              return<h1 key={i}>{m?.messageSent}</h1>
+    <ScrollableFeed className='all-msg-container'>
+      {messages &&
+        messages.map((m, i) => {
+            return (
+              <>
+                {m?.sender?._id === loggedUserId ? (
+                  <div className="chat-msg-container right-side" key={i}>
+                    <div className="chat-bubble-right">
+                      <span className="text-bubble sender">
+                        {m?.messageSent}
+                      </span>
+                      <span className='chat-bubble-time-right'>
+                        {timeDifference(currentTime, new Date(m?.createdAt))}
+                      </span>
+                    </div>
+                  </div>
+                ) : (
+                  <div className="chat-msg-container left-side" key={i}>
+                    <img
+                      src={m?.sender?.profilePic}
+                      alt="user profile"
+                      className="chat-bubble-user"
+                    />
+                    <div className="chat-bubble-left">
+                      <span className="text-bubble receiver">
+                        {m?.messageSent}
+                      </span>
+                      <span className='chat-bubble-time-left'>
+                        {timeDifference(currentTime, new Date(m?.createdAt))}
+                      </span>
+                    </div>
+                  </div>
+                )}
+              </>
+            );
         })}
     </ScrollableFeed>
-  )
+  );
 }
 
 export default DisplayMessagesBox
