f0188063335d129c3e97a04b8e003a37427c9e29
Sam Saryon <ssaryonjr@gmail.com>
2022-09-30 11:09:34 -0700

    trying again

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,16 @@ const server = app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`.bgBlue);
 });
 
+// --------------------------deployment------------------------------
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../frontend/build/")));
+
+  // app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+} else {
+  app.get('/', (req, res) => res.send('Please set to node_env to production'))
+}
+
+// --------------------------deployment------------------------------
 
 //Initiating Socket.io
 const io = require("socket.io")(server, {
@@ -71,14 +81,5 @@ io.on("connection", (socket) => {
   })
 })
 
-// --------------------------deployment------------------------------
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "../frontend/build/")));
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
-} else {
-  app.get('/', (req, res) => res.send('Please set to node_env to production'))
-}
-
-// --------------------------deployment------------------------------
 
